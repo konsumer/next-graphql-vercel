f2414fdeb66c194d16fc72bcfa58b8d758ee2b3f
David Konsumer <konsumer@jetboystudio.com>
2021-06-14 19:34:59 -0700

    record mock tool

---
+++ test/generate_user.js
@@ -0,0 +1,14 @@
+const faker = require('faker')
+const { writeFileSync } = require('fs')
+
+const out = []
+for (let i = 0; i < 10; i++) {
+  out.push({
+    id: faker.datatype.uuid(),
+    login: faker.internet.userName(),
+    avatar_url: faker.image.avatar()
+  })
+}
+
+console.log(out)
+writeFileSync('test/user.json', JSON.stirngify(out, null, 2))
