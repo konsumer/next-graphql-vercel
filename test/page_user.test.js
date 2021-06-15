d351ff94b0b5626fc9bae80c17a42b64e742c3ef
David Konsumer <konsumer@jetboystudio.com>
2021-06-14 19:11:37 -0700

    test for delete

--- test/page_user.test.js
+++ test/page_user.test.js
@@ -1,9 +1,10 @@
 // Integration test for User page
 
-/* global beforeAll afterEach afterAll it expect */
+/* global jest beforeAll afterEach afterAll it expect */
 
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { graphql } from 'msw'
 import { setupServer } from 'msw/node'
 
@@ -22,6 +23,17 @@ handlers.push(graphql.query('GET_USER', (req, res, ctx) => {
   )
 }))
 
+// this allows you to track how the user called the graphql
+const deleteHandler = jest.fn()
+handlers.push(graphql.mutation('DELETE_USER', (req, res, ctx) => {
+  deleteHandler(req.variables?.userId)
+  return res(
+    ctx.data({
+      deleteUser: users.find(row => row.login.toLowerCase() === req.variables?.userId.toLowerCase())
+    })
+  )
+}))
+
 const server = setupServer(...handlers)
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
@@ -35,4 +47,15 @@ it('shows a user', async () => {
   )
   expect(await screen.findByText(users[0].id)).toBeVisible()
   expect(await screen.findByText(users[0].login)).toBeVisible()
+})
+
+it('deletes a user', async () => {
+  render(
+    <MockRouter query={{ userId: users[0].login }}>
+      <PageUser />
+    </MockRouter>
+  )
+  userEvent.click(await screen.findByText('Delete'))
+  expect(deleteHandler.mock.calls.length).toBe(1)
+  expect(deleteHandler.mock.calls[0][0]).toBe(users[0].login)
 })
